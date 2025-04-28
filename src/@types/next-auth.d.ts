65c033b630a0a8fc30adb0e075c009cb05a26250
Adones Balica <adonesbalica@gmail.com>
2025-04-28 20:42:27 -0300

    feat: user auth

---
+++ src/@types/next-auth.d.ts
@@ -0,0 +1,11 @@
+import NextAuth from 'next-auth'
+
+declare module 'next-auth' {
+  interface User {
+    id: string
+    email: string
+    name: string
+    username: string
+    avatar_url: string
+  }
+}
